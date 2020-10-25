9c23345e48c5e9a3a7746f6fb8dbded0e1d11123
Nadya Chistyakova <npodtynchenko@list.ru>
2020-10-25 16:55:49 +0300

    edit cod

--- src/main/java/ru/netology/manager/PosterManager.java
+++ src/main/java/ru/netology/manager/PosterManager.java
@@ -2,42 +2,65 @@ package ru.netology.manager;
 
 import ru.netology.domain.Poster;
 
-public class PosterManager {
-    private Poster[] items = new Poster[0];
+import java.sql.PreparedStatement;
 
-    public void add(Poster item) {
+public class PosterManager {
+
+    private Poster[] films = new Poster[0];
+    private int countFilms = 10;
+
+    public PosterManager() {
+    }
+    public PosterManager(int countFilms) {
+        if (countFilms < 0) {
+            return;
+        }
+        this.countFilms = countFilms;
+    }
+
+    public void add(Poster film) {
 //        создаем новый массив размером на единицу больше
-        int length = items.length + 1;
+        int length = films.length + 1;
         Poster[] tmp = new Poster[length];
 //        копируем поэлементно itar
-        System.arraycopy(items, 0, tmp, 0, items.length);
+        System.arraycopy(films, 0, tmp, 0, films.length);
 //        кладем наш элемент последним
         int lastIndex = tmp.length - 1;
-        tmp[lastIndex] = item;
-        items = tmp;
+        tmp[lastIndex] = film;
+        films = tmp;
     }
 
     public Poster[] getAll() {
-        Poster[] result = new Poster[items.length];
+        Poster[] result = new Poster[films.length];
 //        перебираем массив, но кладем результаты в обратном порядке
         for (int i = 0; i < result.length; i++) {
-            int index = items.length - i - 1;
-            result[i] = items[index];
+            int index = films.length - i - 1;
+            result[i] = films[index];
         }
         return result;
     }
 
     public void removeById(int id) {
-        int length = items.length - 1;
+        int length = films.length - 1;
         Poster[] tmp = new Poster[length];
         int index = 0;
-        for (Poster item: items) {
-            if (item.getId() != id) {
-                tmp[index] = item;
+        for (Poster film: films) {
+            if (film.getId() != id) {
+                tmp[index] = film;
                 index++;
             }
         }
 //        меняем наши элементы
-        items = tmp;
+        films = tmp;
+    }
+
+    public Poster[] getLimit() {
+        int countFilms = films.length;
+        Poster[] limit = new Poster[countFilms];
+        for (int i = 0; i < countFilms; i++) {
+            int index = films.length - i - 1;
+            limit[i] = films[index];
+        }
+        return limit;
     }
 }
--- src/test/java/ru/netology/manager/PosterManagerTest.java
+++ src/test/java/ru/netology/manager/PosterManagerTest.java
@@ -8,46 +8,73 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class PosterManagerTest {
     private PosterManager manager = new PosterManager();
-    private Poster first = new Poster(1,1,"first",1,1);
-    private Poster second = new Poster(2,2,"second",1,1);
-    private Poster third = new Poster(3,3,"third",1,1);
+    private Poster first = new Poster(1,1,"first",0,1);
+    private Poster second = new Poster(2,2,"second",0,1);
+    private Poster third = new Poster(3,3,"third",0,1);
+    private Poster fourth = new Poster(4,4,"fourth",0,1);
+    private Poster fifth = new Poster(5,5,"fifth",0,1);
+    private Poster sixth = new Poster(6,6,"sixth",0,1);
+    private Poster seventh = new Poster(7,7,"seventh",0,1);
+    private Poster eighth = new Poster(8,8,"eighth",0,1);
+    private Poster ninth = new Poster(9,9,"ninth",0,1);
 
     @BeforeEach
     public void setUp() {
         manager.add(first);
         manager.add(second);
         manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+        manager.add(sixth);
+        manager.add(seventh);
+        manager.add(eighth);
+        manager.add(ninth);
     }
 
     @Test
     public void shouldRemoveIfExists() {
-
-        int idToRemove = 1;
-
-        manager.removeById(idToRemove);
+        int id = 1;
+        manager.removeById(id);
 
         Poster[] actual = manager.getAll();
-        Poster[] expected = new Poster[] {third,second};
+        Poster[] expected = new Poster[] {ninth,eighth,seventh,sixth,fifth,fourth,third,second};
 
         assertArrayEquals(expected,actual);
     }
 
-//    @Test
-//    public void shouldNotRemoveIfNotExists() {
-//        PosterManager manager = new PosterManager();
-//        int idToRemove = 4;
-//        Poster first = new Poster(1,1,"first",1,1);
-//        Poster second = new Poster(2,2,"second",1,1);
-//        Poster third = new Poster(3,3,"third",1,1);
-//        manager.add(first);
-//        manager.add(second);
-//        manager.add(third);
-//
-//        manager.removeById(idToRemove);
-//
-//        Poster[] actual = manager.getAll();
-//        Poster[] expected = new Poster[] {third,second,first};
-//
-//        assertArrayEquals(expected,actual);
-//    }
+    @Test
+    public void addFilm() {
+        Poster tenth = new Poster(10,10,"tenth",1,1);
+        manager.add(tenth);
+
+        Poster[] actual = manager.getAll();
+        Poster[] expected = new Poster[] {tenth,ninth,eighth,seventh,sixth,fifth,fourth,third,second,first};
+
+        assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    public void addFilm5() {
+        PosterManager manager = new PosterManager(5);
+
+        Poster[] actual = manager.getAll();
+        Poster[] expected = new Poster[] {ninth,eighth,seventh,sixth,fifth};
+
+        assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    public void getAllFilmFalse() {
+        Poster tenth = new Poster(10,10,"tenth",1,1);
+        Poster eleventh = new Poster(11,11,"eleventh",1,1);
+
+        manager.add(tenth);
+        manager.add(eleventh);
+
+        Poster[] actual = manager.getLimit();
+        Poster[] expected = new Poster[] {eleventh,tenth,ninth,eighth,seventh,sixth,fifth,fourth,third,second};
+
+        assertArrayEquals(expected,actual);
+    }
+
 }
